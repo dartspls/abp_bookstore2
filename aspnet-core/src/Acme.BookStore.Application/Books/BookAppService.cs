634c7d3d9492dfbae9c7350a620af7da6b6ed61b
Dan <darts100@gmail.com>
2022-07-14 18:08:34 +1200

    Created BookAppService class

---
+++ aspnet-core/src/Acme.BookStore.Application/Books/BookAppService.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+using Volo.Abp.Domain.Repositories;
+
+namespace Acme.BookStore.Books
+{
+    public class BookAppService :
+        CrudAppService<
+            Book, // Book entity
+            BookDto, // To show books
+            Guid, // Primary key of the book entity
+            PagedAndSortedResultRequestDto, // Paging and sorting
+            CreateUpdateBookDto>, // Create/update
+        IBookAppService // Implement the interface we created
+    {
+        public BookAppService(IRepository<Book, Guid> repository)
+            : base(repository)
+        {
+
+        }
+    }
+}
