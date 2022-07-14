f4478c14b2a4497ff87d85382c9ea594d5f3418a
Dan <darts100@gmail.com>
2022-07-14 16:51:50 +1200

    Fixed missing semicolon

--- aspnet-core/src/Acme.BookStore.Domain/Books/Book.cs
+++ aspnet-core/src/Acme.BookStore.Domain/Books/Book.cs
@@ -6,7 +6,7 @@ namespace Acme.BookStore.Books
     public class Book : AuditedAggregateRoot<Guid>
     {
         public string Name { get; set; }
-        public BookType Type { get; set }
+        public BookType Type { get; set; }
         public DateTime PublishDate { get; set; }
         public float Price { get; set; }
     }
