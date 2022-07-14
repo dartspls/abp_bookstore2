429a8e534978b88d767640c3ddb417fefcca1743
Dan <darts100@gmail.com>
2022-07-14 17:58:36 +1200

    Created interface for application service

---
+++ aspnet-core/src/Acme.BookStore.Application.Contracts/Books/IBookAppService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using Volo.Abp.Application.Dtos;
+using Volo.Abp.Application.Services;
+
+namespace Acme.BookStore.Books
+{
+    public interface IBookAppService :
+        ICrudAppService< // Defines CRUD methods
+            BookDto, // Used to show books
+            Guid, // primary key of book entity
+            PagedAndSortedResultRequestDto, // used for paging/sorting
+            CreateUpdateBookDto> // used to create/update a book
+    {
+
+    }
+}
