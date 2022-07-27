c319e5f643112056eb812b25c3fb072f31eddde7
Daniel Shepherd <daniel.shepherd@atcltd.atc.org.nz>
2022-07-27 20:25:10 +1200

    Created author already exists exception class

---
+++ aspnet-core/src/Acme.BookStore.Domain/Authors/AuthorAlreadyExistsException.cs
@@ -0,0 +1,14 @@
+﻿using Volo.Abp;
+
+namespace Acme.BookStore.Authors
+{
+    internal class AuthorAlreadyExistsException : BusinessException
+    {
+        public AuthorAlreadyExistsException(string name)
+            : base(BookStoreDomainErrorCodes.AuthorAlreadyExists)
+        {
+            WithData("name", name);
+
+        }
+    }
+}
