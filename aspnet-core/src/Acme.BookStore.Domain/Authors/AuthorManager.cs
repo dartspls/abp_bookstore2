af874c49edc7fef495dbe74e0966c9f4ce2c1d17
Daniel Shepherd <daniel.shepherd@atcltd.atc.org.nz>
2022-07-27 20:31:31 +1200

    Added AuthorManager class

---
+++ aspnet-core/src/Acme.BookStore.Domain/Authors/AuthorManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Threading.Tasks;
+using JetBrains.Annotations;
+using Volo.Abp;
+using Volo.Abp.Domain.Services;
+
+namespace Acme.BookStore.Authors
+{
+    public class AuthorManager : DomainService
+    {
+        private readonly IAuthorRepository _authorRepository;
+
+        public AuthorManager (IAuthorRepository authorRepository)
+        {
+            _authorRepository = authorRepository;
+        }
+
+        public async Task<Author> CreateAsync(
+            [NotNull] string name,
+            DateTime birthDate,
+            [CanBeNull] string shortBio = null)
+        {
+            Check.NotNullOrWhiteSpace(name, nameof(name));
+
+            // if an author already exists with this name then throw an exception
+            var existingAuthor = await _authorRepository.FindByNameAsync(name);
+            if(existingAuthor != null)
+            {
+                throw new AuthorAlreadyExistsException(name);
+            }
+
+            // otherwise go ahead and create a new author
+            return new Author(
+                GuidGenerator.Create(),
+                name,
+                birthDate,
+                shortBio
+            );
+        }
+
+        public async Task ChangeNameAsync (
+            [NotNull] Author author,
+            [NotNull] string newName)
+        {
+            Check.NotNull(author, nameof(author));
+            Check.NotNullOrWhiteSpace(newName, nameof(newName));
+
+            // check if another author already exists with the name attempting to be changed to
+            var existingAuthor = await _authorRepository.FindByNameAsync(newName);
+            if (existingAuthor != null && existingAuthor.Id != author.Id)
+            {
+                throw new AuthorAlreadyExistsException(newName);
+            }
+
+            // otherwise go ahead and change
+            author.ChangeName(newName);
+        }
+    }
+}
